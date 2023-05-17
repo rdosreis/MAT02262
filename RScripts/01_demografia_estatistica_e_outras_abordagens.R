b9b72bd1da5e9b6259346cbd89de44eb1e526d99
rdosreis <rodrigocpdosreis@gmail.com>
2023-05-16 22:08:00 -0300

    Aula 01

---
+++ RScripts/01_demografia_estatistica_e_outras_abordagens.R
@@ -0,0 +1,41 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'tamanho_pop.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='70%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'densidade_e_dist_pop.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='70%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'piramide_etaria_pop.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'crescimento_pop.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'obitos_nascimentos_migracoes_pop.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'transicao_demografica_5_estagios.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'Statistically-Insignificant-corda-bamba.jpg'))
+
+
