c6939de2444af9479e13321c82d0eafe439d4c28
rdosreis <rodrigocpdosreis@gmail.com>
2023-05-23 16:39:00 -0300

    pequenas correções

--- RScripts/00_apresenta_curso.R
+++ RScripts/00_apresenta_curso.R
@@ -1,9 +1,9 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='65%', paged.print=FALSE------------------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='65%', paged.print=FALSE----------------
 
 knitr::include_graphics(here::here('imagens','R-Excel.jpg'))
 
 
 
-## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', paged.print=FALSE-------------------------
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', paged.print=FALSE-----------------
 knitr::include_graphics(here::here('imagens','ctanlion.png'))
 
