b022415d9244651a531c7b7cabed74fd2aab359a
RODRIGO REIS <rodrigocpdosreis@gmail.com>
2023-05-29 17:01:42 -0300

    Aula 02

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "00_apresenta_curso"
+arquivo <- "02_transicao_demografica"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
