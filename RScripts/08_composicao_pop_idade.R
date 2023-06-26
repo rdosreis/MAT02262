25a43b5fb4208e41a2ca41525223d94c899b3584
rdosreis <rodrigocpdosreis@gmail.com>
2023-06-26 17:16:14 -0300

    Aula 08

---
+++ RScripts/08_composicao_pop_idade.R
@@ -0,0 +1,23 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='60%', out.height='40%'----
+
+knitr::include_graphics(here::here('imagens', 'piramide_etaria_pop.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='70%', out.height='70%', fig.cap='Exemplo de uma pirâmide etária com os dados do Censo de Angola, 2014.'----
+
+knitr::include_graphics(here::here('imagens', 'piramide_angola.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='60%', out.height='50%', fig.cap='População por sexo e idade (distribuição relativa): Exemplo de uma pirâmide etária com os dados do Censo de Macau, 2011.'----
+
+knitr::include_graphics(here::here('imagens', 'piramide_macau.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'Statistically-Insignificant-barras05.jpg'))
+
+
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "07_fontes_de_dados_demo_la"
+arquivo <- "08_composicao_pop_idade"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
