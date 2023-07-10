b57b249a622f99c96a3417b07792c4831c2fd3d6
RODRIGO REIS <rodrigocpdosreis@gmail.com>
2023-07-10 17:07:51 -0300

    Aula 12

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "11_composicao_pop_padronizacao"
+arquivo <- "12_eventos_proporcoes_taxas_probs"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
