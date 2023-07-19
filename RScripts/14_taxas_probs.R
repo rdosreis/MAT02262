d23d4f76bbdcc4a3de31f47974afb93891bf9140
RODRIGO REIS <rodrigocpdosreis@gmail.com>
2023-07-19 17:46:31 -0300

    Aula 14 parte 2

--- RScripts/14_taxas_probs.R
+++ RScripts/14_taxas_probs.R
@@ -16,6 +16,36 @@ knitr::include_graphics(here::here('imagens', 'pessoas-ano_02.png'))
 
 
 
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height="50%"----------------------------------------------------------------
+
+knitr::include_graphics(here::here('imagens', 'pessoas-ano_03.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height="50%"----------------------------------------------------------------
+
+knitr::include_graphics(here::here('imagens', 'pessoas-ano_04.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height="50%"----------------------------------------------------------------
+
+knitr::include_graphics(here::here('imagens', 'pessoas-ano_05.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height="50%"----------------------------------------------------------------
+
+knitr::include_graphics(here::here('imagens', 'pessoas-ano_06.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height="50%"----------------------------------------------------------------
+
+knitr::include_graphics(here::here('imagens', 'pessoas-ano_07.png'))
+
+
+
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%'----------------------------------------------------------------
 
 knitr::include_graphics(here::here('imagens', 'Statistically-Insignificant-final01.jpg'))
