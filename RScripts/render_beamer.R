56050c05f867be29e237116ec7533118fad58449
RODRIGO REIS <rodrigocpdosreis@gmail.com>
2023-08-21 16:42:44 -0300

    Aula 19

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "18_tabuas_vida"
+arquivo <- "19_tabuas_vida"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
