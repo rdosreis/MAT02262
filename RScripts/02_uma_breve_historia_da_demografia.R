dc6d0cb8263baad4a4a808f05043a9c60b480c5d
rdosreis <rodrigocpdosreis@gmail.com>
2024-03-22 21:33:01 -0300

    breve história - p2

--- RScripts/02_uma_breve_historia_da_demografia.R
+++ RScripts/02_uma_breve_historia_da_demografia.R
@@ -28,6 +28,54 @@ knitr::include_graphics(here::here('imagens', 'Quipu.png'))
 
 
 
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'JG.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='left', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'edmond_halley.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'richard_price.jpeg'))
+
+
+
+## ----echo=FALSE, fig.align='left', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'BenFranklinDuplessis.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'MalthusThomas300px.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='left', message=FALSE, warning=FALSE, out.width='40%', out.height='30%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'gauss-german-mark.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'Laplace.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='50%', out.height='45%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'United-Nations-Poster.jpg'))
+
+
+
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', out.height='75%', paged.print=FALSE----
 
 knitr::include_graphics(here::here('imagens', 'teorias_demograficas.jpg'))
