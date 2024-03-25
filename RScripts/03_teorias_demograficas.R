59bcae57e27a414b128b55c66532fb1d042ece35
rdosreis <rodrigocpdosreis@gmail.com>
2024-03-25 01:08:39 -0300

    Algumas teorias

---
+++ RScripts/03_teorias_demograficas.R
@@ -0,0 +1,47 @@
+## ----echo=FALSE, fig.align='left', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'william-godwin-collection.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'condorcet.jpeg'))
+
+
+
+## ----echo=FALSE, fig.align='left', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'MalthusThomas300px.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', out.height='70%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'teoria-malthusiana.png'))
+
+
+
+## ----echo=FALSE, fig.align='left', message=FALSE, warning=FALSE, out.width='15%', out.height='23%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'karl_marx_cropped.png'))
+
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'population-explosion.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='75%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'clube_roma.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', out.height='75%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('imagens', 'Model-of-demographic-transition.png'))
+
+
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "02_uma_breve_historia_da_demografia"
+arquivo <- "03_teorias_demograficas"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
