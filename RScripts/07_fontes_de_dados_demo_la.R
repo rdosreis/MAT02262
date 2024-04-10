1ceefabf5d324c39df97ecc88e6cac9caaf677da
rdosreis <rodrigocpdosreis@gmail.com>
2024-04-10 12:12:21 -0300

    aula 07 - fim da área 01

--- RScripts/07_fontes_de_dados_demo_la.R
+++ RScripts/07_fontes_de_dados_demo_la.R
@@ -1,11 +1,5 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', out.height='80%'-------------------------
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', out.height='80%'----
 
 knitr::include_graphics(here::here('imagens', 'mobilidade.png'))
-
-
-
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE------
-
-knitr::include_graphics(here::here('imagens', 'Statistically-Insignificant-barras04.jpg'))
 
 
